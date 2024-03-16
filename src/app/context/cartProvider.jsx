ee38de05ffa905b78bd49adef304b3fc10622b84
Githubmagali <magalivictoria85068@gmail.com>
2024-03-16 15:50:16 -0300

    add localstorage

--- src/app/context/cartProvider.jsx
+++ src/app/context/cartProvider.jsx
@@ -4,14 +4,17 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        // Try to load the cart from local storage, or default to an empty array
+        const savedCart = localStorage.getItem('cart');
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
     const [totalCost, setTotalCost] = useState(0);
 
     const getItemQuantity = (itemId) => {
         const item = cart.find((cartItem) => cartItem.id === itemId);
         return item ? item.quantity : 0;
     };
-
 
     const addToCart = (item) => {
         setCart((prevCart) => {
@@ -30,12 +33,9 @@ export const CartProvider = ({ children }) => {
 
     const getItemTotalPrice = (itemId) => {
         const item = cart.find((cartItem) => cartItem.id === itemId);
-        return item ? (item.price * item.quantity) : 0;
+        return item ? (item.price * item.quantity).toFixed(2) : '0.00';
     };
 
-
-
-    
     const removeFromCart = (itemId) => {
         setCart((prevCart) => {
             const itemExists = prevCart.find((cartItem) => cartItem.id === itemId);
@@ -50,29 +50,32 @@ export const CartProvider = ({ children }) => {
             }
         });
     };
+
     useEffect(() => {
+        // This effect updates the total cost whenever the cart changes
         const updateTotalCost = () => {
-            const total = cart.reduce((acc, currentItem) => {
-                return acc + currentItem.price * currentItem.quantity;
-            }, 0);
+            const total = cart.reduce((acc, currentItem) => acc + currentItem.price * currentItem.quantity, 0);
             setTotalCost(parseFloat(total.toFixed(2)));
         };
         updateTotalCost();
-        
     }, [cart]);
-    
+
+    useEffect(() => {
+        // This effect saves the cart to local storage whenever the cart changes
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+
     return (
         <CartContext.Provider
-            value={{ cart, addToCart,removeFromCart, totalCost, getItemQuantity, getItemTotalPrice}}
+            value={{ cart, addToCart, removeFromCart, totalCost, getItemQuantity, getItemTotalPrice }}
         >
             {children}
         </CartContext.Provider>
     );
-
 };
 
 export const useCart = () => {
     return useContext(CartContext);
-  };
+};
 
   
